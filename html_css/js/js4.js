026aca867a68695e013660d3c3e443cee5ae2aea
nicolas33450 <40290258+nicolas33450@users.noreply.github.com>
2018-07-12 18:00:39 +0200

    dd

--- html_css/js/js4.js
+++ html_css/js/js4.js
@@ -10,11 +10,10 @@ document.getElementById('nb1').addEventListener('keyup', copier);
 */
 
 
-/*
 
 function changer()
 {
-     var majus = document.getElementById('nb1').value.toUpperCase();
+    /* var majus = document.getElementById('nb1').value.toUpperCase();
     document.getElementById('nb1').value = majus;
 
     var l = document.getElementById('nb1').value.length;
@@ -23,29 +22,28 @@ function changer()
 
     var y = document.getElementById('nb1').value.substr(1,l).toLowerCase();
 
-    document.getElementById('nb1').value = x + y;
+    document.getElementById('nb1').value = x + y;*/
+    
 
     document.getElementById('nb1').value = document.getElementById('nb1').value.substr(0,1).toUpperCase() + document.getElementById('nb1').value.substr(1, document.getElementById('nb1').value.length -1).toLowerCase();
 
-
 }
 
-
 document.getElementById('nb1').addEventListener('change', changer);
-*/
 
-function recup()
+/*function recup()
 {
+    
     var tabinfo = document.getElementsByName('nb3');
-    var contenu = tabinfo[0].value;    
+    var contenu = tabinfo[0].value;
+    
+    
     
     window.alert(contenu);
 }
 
 
-document.getElementById('btn1').addEventListener('click', recup);                                            
-
-
+document.getElementById('btn1').addEventListener('click', recup); */  
 
 
 
